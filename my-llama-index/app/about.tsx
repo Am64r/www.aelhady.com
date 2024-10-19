1ed2c5868478b80f6b1ce4de949f107708a2ed43
Amr Elhady <theamrelhady@gmail.com>
2024-10-18 20:24:11 -0400

    about page

---
+++ my-llama-index/app/about.tsx
@@ -0,0 +1,14 @@
+// pages/about.tsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>About Me</h1>
+      <p>This is where you can describe yourself in detail. Add your bio, skills, background, etc.</p>
+      {/* Add more detailed content as needed */}
+    </div>
+  );
+};
+
+export default About;
--- my-llama-index/app/components/Header.js
+++ my-llama-index/app/components/Header.js
@@ -1,34 +1,34 @@
 "use client";
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import '../css/Header.css';
 
-const Header = ({ parallaxRef  }) => {
+const Header = ({ parallaxRef }) => {
     const [showEmail, setShowEmail] = useState(false);
-    
+
     const scrollToBottom = () => {
         if (parallaxRef.current) {
-          
-          parallaxRef.current.scrollTo(2);
+            parallaxRef.current.scrollTo(2);
         }
-      };
+    };
 
-      const scrollToTop = () => {
+    const scrollToTop = () => {
         if (parallaxRef.current) {
-          
-          parallaxRef.current.scrollTo(0);
+            parallaxRef.current.scrollTo(0);
         }
-      };
- 
+    };
 
     return (
         <header className="header">
             <button className="header-name" onClick={scrollToTop}>A | E</button>
             <nav>
-                {/* <button className="header-link">About</button> */}
+                <button className="header-link">Projects</button>
+                {/* Use Link to route to the About page */}
+                <Link href="/about" passHref>
+                    <button className="header-link">About</button>
+                </Link>
                 <button className="header-link" onClick={scrollToBottom}>Contact</button>
-
-
             </nav>
             {showEmail && (
                 <div className="email-box">
